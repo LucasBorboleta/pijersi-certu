7a7c092a252a35c8386fa3b2352dbaf54da44335
Lucas Borboleta <lucas.borboleta@free.fr>
2022-12-01 22:09:06 +0100

    Update proto_engine.py

--- docs/proto_engine.py
+++ docs/proto_engine.py
@@ -66,12 +66,12 @@ class GameState:
     
 @dataclass
 class Action:
-    path: Path
+    path_summary: Path
     new_board_state: BoardState
     has_capture: bool
      
     
-def get_sources(board_state: BoardState, current_player: Player) -> list[HexIndex]:
+def find_cube_sources(board_state: BoardState, current_player: Player) -> list[HexIndex]:
     sources = list()
     for (hex_index, hex_state) in enumerate(board_state):
          if not hex_state.empty and hex_state.player == current_player:
@@ -79,51 +79,51 @@ def get_sources(board_state: BoardState, current_player: Player) -> list[HexInde
     return sources
 
 
-def get_path1(source: HexIndex, translation: Translation1) -> Path:
+def make_path1(source: HexIndex, translation: Translation1) -> Path:
     path = list()
     path.append(source)
     return path
 
 
-def get_path2(source: HexIndex, translation: Translation2) -> Path:
+def make_path2(source: HexIndex, translation: Translation2) -> Path:
     path = list()
     path.append(source)
     return path
 
 
-def get_path_state(board_state: BoardState, path: Path) -> PathState:
+def make_path_state(board_state: BoardState, path: Path) -> PathState:
     path_state = [board_state[hex_index] for hex_index in path]
     return path_state
 
 
-def try_path_state(path_state: PathState) -> (PathState, bool):
+def try_cube_path(path_state: PathState) -> (PathState, bool):
     new_path_state = None
     has_capture = False
     return (new_path_state, has_capture)
 
 
-def apply_path_state(board_state: BoardState, path: Path, path_state: PathState) -> (BoardState, bool):
+def apply_path_state(board_state: BoardState, path: Path, path_state: PathState) -> BoardState:
     new_board_state = copy.deepcopy(board_state)
     for (hex_index, hex_state) in zip(path, path_state):
         new_board_state[hex_index] = hex_state
+    return new_board_state
 
     
-def get_actions(game_state: GameState) -> list[Action]:
+def find_actions(game_state: GameState) -> list[Action]:
     actions = list()
     if not game_state.terminal:
         board_state = game_state.board_state
         current_player = game_state.current_player
         
-        for source in get_sources(board_state, current_player):
+        for source in find_cube_sources(board_state, current_player):
             for translation in Translation1:
-                path = get_path1(source, translation)
+                path = make_path1(source, translation)
                 if len(path) != 0:
-                    path_state = get_path_state(board_state, path)
-                    (new_path_state, has_capture) = try_path_state(path_state)
+                    path_state = make_path_state(board_state, path)
+                    (new_path_state, has_capture) = try_cube_path(path_state)
                     if len(new_path_state) != 0:
                         action = Action()
-                        destination = path[-1]
-                        action.path = [source, destination]
+                        action.path_summary = [source, path[-1]]
                         action.new_board_state = apply_path_state(board_state, path, new_path_state)
                         action.has_capture = has_capture                       
                         actions.append(action)        
