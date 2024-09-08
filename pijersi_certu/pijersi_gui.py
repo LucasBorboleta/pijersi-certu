fbcfa69ea5e5228536b24ea4795eea941489626c
Lucas Borboleta <lucas.borboleta@free.fr>
2024-09-08 15:15:01 +0200

    Keep simple statistics for review

--- pijersi_certu/pijersi_gui.py
+++ pijersi_certu/pijersi_gui.py
@@ -2072,30 +2072,17 @@ class GameGui(ttk.Frame):
 
         else:
 
+            # copute and show simple statistics
             white_reviews = self.__turn_reviews[1::2]
             black_reviews = self.__turn_reviews[2::2]
 
-            white_unseens = len([grade for grade in white_reviews if grade is not None and grade == -1])
             white_grades = [grade for grade in white_reviews if grade is not None and grade != -1]
-            white_grade_min = min(white_grades) if len(white_grades) != 0 else '*'
-            white_grade_max = max(white_grades) if len(white_grades) != 0 else '*'
             white_grade_ave = int(sum(white_grades)/len(white_grades)) if len(white_grades) != 0 else '*'
 
-            black_unseens = len([grade for grade in black_reviews if grade is not None and grade == -1])
             black_grades = [grade for grade in black_reviews if grade is not None and grade != -1]
-            black_grade_min = min(black_grades) if len(black_grades) != 0 else '*'
-            black_grade_max = max(black_grades) if len(black_grades) != 0 else '*'
             black_grade_ave = int(sum(black_grades)/len(black_grades)) if len(black_grades) != 0 else '*'
 
-            if True:
-                # simple statistics
-                self.__variable_log.set(f"review done ; white  average={white_grade_ave}  /  black  average={black_grade_ave}")
-            else:
-                # more elaborated statistics
-                self.__variable_log.set("review done ; " +
-                                        f"white  average={white_grade_ave}  min={white_grade_min}  max={white_grade_max}  #?={white_unseens}" +
-                                        " / " +
-                                        f"black  average={black_grade_ave}  min={black_grade_min}  max={black_grade_max}  #?={black_unseens}")
+            self.__variable_log.set(f"review done ; white  average={white_grade_ave}  /  black  average={black_grade_ave}")
             self.__review_in_progress = False
 
         if not self.__review_in_progress:
